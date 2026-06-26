4d9c17933318886bbfab2e1129541bdadd188ea7
jorgelima-maker <jorge.lima@ifpi.edu.br>
2026-06-26 20:58:40 -0300

    Implement Conta class with deposit and withdraw methods

--- Conta.java
+++ Conta.java
@@ -1,1 +1,18 @@
+public class Conta {
+    private double saldo;
 
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) throws SaldoException {
+        if (valor > this.saldo) {
+            throw new SaldoException();
+        }
+        this.saldo -= valor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
